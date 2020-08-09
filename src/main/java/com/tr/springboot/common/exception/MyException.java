2a2fcfe622556ff134eaf1e97f8fec276f3aea74
taorun <tr1838@163.com>
2020-08-09 23:51:59 +0800

    0

--- src/main/java/com/tr/springboot/common/exception/MyException.java
+++ src/main/java/com/tr/springboot/common/exception/MyException.java
@@ -4,38 +4,37 @@ import com.tr.springboot.common.result.ResultEnum;
 
 /**
  * 自定义异常
- * 
+ *
  * @author taorun
  * @date 2017年11月21日 下午3:24:53
- *
  */
 public class MyException extends RuntimeException {
 
-	private static final long serialVersionUID = 2463545557484894315L;
-	
-	private Integer code;
-	
+    private static final long serialVersionUID = 2463545557484894315L;
+
+    private Integer code;
+
     public MyException(ResultEnum resultEnum) {
         super(resultEnum.getMsg());
         this.code = resultEnum.getCode();
     }
-    
+
     public MyException(ResultEnum resultEnum, Exception exception) {
-		super(resultEnum.getMsg() + " : " + exception.getMessage());
-		this.code = resultEnum.getCode();
+        super(resultEnum.getMsg() + " : " + exception.getMessage());
+        this.code = resultEnum.getCode();
     }
 
     public MyException(String string) {
-    		super(string);
-    		this.code = ResultEnum.FAIL.getCode();
-	}
+        super(string);
+        this.code = ResultEnum.FAIL.getCode();
+    }
 
-	public MyException(Integer code, String string) {
-		super(string);
-		this.code = code;
-	}
+    public MyException(Integer code, String string) {
+        super(string);
+        this.code = code;
+    }
 
-	public Integer getCode() {
+    public Integer getCode() {
         return code;
     }
 
