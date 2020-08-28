bc60c51f5298ae3b673f45b742a63c659558af00
taorun <tr1838@163.com>
2020-08-28 23:26:34 +0800

    0

---
+++ src/main/java/com/tr/springboot/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package com.tr.springboot.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ 配置类
+ *
+ * @author TR
+ * @version 1.0
+ * @date 2020/8/25 下午9:38
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    /**
+     * 创建一个名为 message 的队列
+     */
+    @Bean
+    public Queue messageQueue(){
+        return new Queue("message");
+    }
+
+}
---
+++ src/main/java/com/tr/springboot/rabbitmq/Sender.java
@@ -0,0 +1,31 @@
+package com.tr.springboot.rabbitmq;
+
+import org.apache.log4j.Logger;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * 消息发送者Sender
+ * 使用AmqpTemplate将消息发送到消息队列message中去
+ *
+ * @author TR
+ * @version 1.0
+ * @date 2020/8/25 下午7:31
+ */
+@Component
+public class Sender {
+    Logger logger = Logger.getLogger(Sender.class);
+
+    @Resource
+    private AmqpTemplate amqpTemplate;
+
+    public String send() {
+        String context = "Test Message";
+        logger.info("发送消息：" + context + "  时间：" + new Date());
+        amqpTemplate.convertAndSend("message", context);
+        return "发送成功";
+    }
+}
