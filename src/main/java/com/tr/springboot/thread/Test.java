9f0dc91e9f4792f29413a3d2f179acb4308dce77
Air-TR <tr1838@163.com>
2020-09-10 11:08:44 +0800

    Thread

--- src/main/java/com/tr/springboot/thread/Test.java
+++ src/main/java/com/tr/springboot/thread/Test.java
@@ -2,7 +2,9 @@ package com.tr.springboot.thread;
 
 import com.tr.springboot.thread.service.ThreadService;
 import com.tr.springboot.thread.service.impl.ThreadServiceImpl;
-import com.tr.springboot.web.entity.Account;
+import com.tr.springboot.web.entity.Entity;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author TR
@@ -19,29 +21,39 @@ public class Test {
 //        String c = threadService.methodC();
 //        System.out.println(a + b + c + "\n");
 
-        Account account = new Account();
+        long start = System.currentTimeMillis();
+
+        final CountDownLatch latch = new CountDownLatch(3);
+
+        Entity entity = new Entity();
 
         Thread t1 = new Thread(() -> {
-            account.setUsername(account.getUsername() + threadService.methodA());
-            System.out.println("T1 - Username: " + account.getUsername());
+            entity.setV1(threadService.methodA());
+            latch.countDown();
         });
         t1.start();
+//        t1.join();
 
         Thread t2 = new Thread(() -> {
-            account.setUsername(account.getUsername() + threadService.methodB());
-            System.out.println("T2 - Username: " + account.getUsername());
+            entity.setV2(threadService.methodB());
+            latch.countDown();
         });
         t2.start();
+//        t2.join();
 
         Thread t3 = new Thread(() -> {
-            account.setUsername(account.getUsername() + threadService.methodC());
-            System.out.println("T3 - Username: " + account.getUsername());
+            entity.setV3(threadService.methodC());
+            latch.countDown();
         });
         t3.start();
-        t3.join();
+//        t3.join();
 
-        System.out.println("Username: " + account.getUsername());
+        latch.await();
+//        Thread.sleep(5000);
+        System.out.println(entity.toString());
 
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (end - start) + "ms");
 
 //        new Thread(() -> threadService.methodA()).start();
 //        new Thread(() -> threadService.methodB()).start();
