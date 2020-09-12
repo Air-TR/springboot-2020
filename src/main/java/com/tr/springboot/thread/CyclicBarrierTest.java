fdac8ff49811c9f0136829b4cd92a534541561e4
taorun <tr1838@163.com>
2020-09-13 01:27:08 +0800

    0

--- src/main/java/com/tr/springboot/thread/CyclicBarrierTest.java
+++ src/main/java/com/tr/springboot/thread/CyclicBarrierTest.java
@@ -1,12 +1,70 @@
 package com.tr.springboot.thread;
 
+import com.tr.springboot.thread.service.ThreadService;
+import com.tr.springboot.thread.service.impl.ThreadServiceImpl;
+import com.tr.springboot.web.entity.Entity;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
+ * CyclicBarrier 测试
+ * CyclicBarrier 要比 CountDownLatch 多1
+ * 比如: 开三个线程,CountDownLatch配3,CyclicBarrier要配4
+ *
  * @author TR
  * @version 1.0
  * @date 9/10/2020 1:18 PM
  */
 public class CyclicBarrierTest {
 
+    private static ThreadService threadService = new ThreadServiceImpl();
 
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
+        long start = System.currentTimeMillis();
+
+        final CyclicBarrier barrier = new CyclicBarrier(4);
+
+        Entity entity = new Entity();
+
+        new Thread(() -> {
+            entity.setV1(threadService.methodA());
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            entity.setV2(threadService.methodB());
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            entity.setV3(threadService.methodC());
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        barrier.await();
+        System.out.println(entity.toString());
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (end - start) + "ms");
+    }
 
 }
