413651e0d209f96e0d50dace2b88da396f3b07cd
taorun <tr1838@163.com>
2022-01-24 19:00:05 +0800

    1

--- src/main/java/com/tr/springboot/async/AsyncController.java
+++ src/main/java/com/tr/springboot/async/AsyncController.java
@@ -1,19 +1,27 @@
-//package com.tr.springboot.async;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * @author rtao
-// * @date 2022/1/24 15:53
-// */
-//@RestController
-//public class AsyncController {
-//
-//    @GetMapping("/async")
-//    public void async() throws InterruptedException {
-//        Thread.sleep(3000);
-//        System.out.println(Thread.currentThread().getName() + ": async success");
-//    }
-//
-//}
+package com.tr.springboot.async;
+
+import com.tr.springboot.async.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 异步测试类
+ *
+ * @author rtao
+ * @date 2022/1/24 15:53
+ */
+@RestController
+public class AsyncController {
+
+    @Autowired
+    AsyncService asyncService;
+
+    @GetMapping("/async")
+    public void async() {
+        System.out.println("start");
+        asyncService.async();
+        System.out.println("end");
+    }
+
+}
---
+++ src/main/java/com/tr/springboot/async/service/AsyncService.java
@@ -0,0 +1,24 @@
+package com.tr.springboot.async.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author TR
+ * @date 2022/1/24 下午6:46
+ */
+@Service
+public class AsyncService {
+
+    @Async // 使用 @Async 需要在启动类配置 @EnableAsync
+    public void async() {
+        try {
+            System.out.println(Thread.currentThread().getName() + ": async start");
+            Thread.sleep(3000);
+            System.out.println(Thread.currentThread().getName() + ": async end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
