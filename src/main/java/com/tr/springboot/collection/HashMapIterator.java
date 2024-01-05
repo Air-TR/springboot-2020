59d821bc129ff40e0d68d3be4f3d171c001d0a83
taorun <tr1838@163.com>
2024-01-05 17:41:53 +0800

    1

--- src/main/java/com/tr/springboot/collection/HashMapIterator.java
+++ src/main/java/com/tr/springboot/collection/HashMapIterator.java
@@ -9,8 +9,8 @@ import java.util.Map;
  * 1.使用 EntrySet 遍历
  * 2.使用 KeySet 遍历
  *
- * @Author TR
  * @version 1.0
+ * @Author TR
  * @date 9/7/2020 2:57 PM
  */
 public class HashMapIterator {
@@ -26,27 +26,27 @@ public class HashMapIterator {
 
     public static void testHashMapIteratorByEntrySet() {
         Map map = createMap();
-        long startTime  = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         System.out.println("Start: " + startTime);
         hashMapIteratorByEntrySet(map);
-        long endTime  = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
         System.out.println("End: " + endTime);
         System.out.println("Time: " + (endTime - startTime) + "ms");
     }
 
     public static void testHashMapIteratorByKeySet() {
         Map map = createMap();
-        long startTime  = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         System.out.println("Start: " + startTime);
         hashMapIteratorByKeySet(map);
-        long endTime  = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
         System.out.println("End: " + endTime);
         System.out.println("Time: " + (endTime - startTime) + "ms");
     }
 
     public static void hashMapIteratorByEntrySet(Map map) {
         Iterator iterator = map.entrySet().iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry entry = (Map.Entry) iterator.next();
             Object key = entry.getKey();
             Object value = entry.getValue();
@@ -56,7 +56,7 @@ public class HashMapIterator {
 
     public static void hashMapIteratorByKeySet(Map map) {
         Iterator iterator = map.keySet().iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Object key = iterator.next();
             Object value = map.get(key);
 //            System.out.println(key + " -> " + value);
@@ -66,7 +66,7 @@ public class HashMapIterator {
     public static Map createMap() {
         Map map = new HashMap();
         for (int i = 1; i <= 1000000; i++) {
-            map.put(i, i+1);
+            map.put(i, i + 1);
         }
         return map;
     }
